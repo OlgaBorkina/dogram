b6380982afe1aba1081cbfc01a95d207f95bc7db
OlgaBorkina <ovborkina@gmail.com>
2022-01-09 13:53:55 +0200

    HW #09

--- src/index.js
+++ src/index.js
@@ -1,44 +1,55 @@
 console.log("script launched");
 let detailsImage = document.querySelector(".details-image");
-let detailsTitle = document.querySelector (".details-title");
-let mainContentEl = document.querySelector (".main-content");
-let anchors = document.querySelectorAll (".thumbnails-anchor"); // все эементы HTML которые принадлежат к этому классу
+let detailsTitle = document.querySelector(".details-title");
+let mainContentEl = document.querySelector(".main-content");
+// let audioAnchor;
 let selectedItem;
-
-for(let i=0; i< anchors.length; i++) {
-    anchors[i].addEventListener("click", function(event){ 
-        event.preventDefault(); // отмена дефол - event
-        //console.log(anchors[i].getAttribute("href"));// дай мне значение атрибута/ мы перехватываем клик
-        console.log( anchors[i]);
-        showDetals();
-        setDetails ( anchors[i]); //функция вызова 
-
-    } )
+let anchors = 
+document.querySelectorAll(".thumbnails-anchor");//all HTML elements belonging to the clas thumbnails-anchor
+for(let i = 0; i < anchors.length; i++) {
+    anchors[i].addEventListener("click", function(event) {
+        event.preventDefault(); //canceling default behavior of anchor element hitting
+        showDetails();
+        setDetails(anchors[i]); //setDetails function call with passing reference to appropriate anchor
+    })
 }
 function setDetails(anchor) {
     console.log("anchor element  was pressed", anchor);
-    let hrefValue = anchor.getAttribute("href")
-    detailsImage.setAttribute("src",hrefValue);
-   // detailsTitle.textContent = anchor.getAttribute("data-details-title");
-   // выгрузка элемента с класса details-title внутри этого анкора
-   let thumbnailsTitleSelector = `[href="${hrefValue}"] .thumbnails-title`;
-    let thumbnailTitleEl = document.querySelector(thumbnailsTitleSelector);
-    detailsTitle.textContent = `${thumbnailTitleEl.textContent}: ${anchor.getAttribute('data-details-title')}`;
-    anchor.parentElement.classList.add("selected");
-    if (selectedItem){
-        selectedItem.classList.remove("selected");
-    }
-    selectedItem = anchor.parentElement;
-}
-
-function showDetals () {
-    mainContentEl.classList.remove('hidden');
+    let hrefValue = anchor.getAttribute("href");
+    detailsImage.setAttribute("src", hrefValue );
+    
    
-}
-function hideDetails () {
-    mainContentEl.classList.add('hidden');
-    if (selectedItem){
-        selectedItem.classList.remove("selected");
+    if (selectedItem) {
+        selectedItem.classList.remove("selected")
     }
+    anchor.parentElement.classList.add("selected");
+    selectedItem = anchor.parentElement;
 
+    let mp3Selector = `[href="${hrefValue}"] .myMp3`;
+    let mp3El = document.querySelector(mp3Selector);
+    let mp3Link = mp3El.getAttribute("src");
+    let audioAnchor = new Audio(mp3Link);
+    audioAnchor.play();
+
+    let thumbnailsTitleSelector = `[href="${hrefValue}"] .thumbnails-title`;
+    let thumbnailsTitleEl = document.querySelector(thumbnailsTitleSelector);
+    
+    detailsTitle.textContent = `${thumbnailsTitleEl.textContent}: ${anchor.getAttribute('data-details-title')}` ;   
+}
+function showDetails() {
+    mainContentEl.classList.remove('hidden');
+    detailsImage.parentElement.classList.add('is-tiny');
+    setTimeout(removeIsTiny);
+}   
+    
+
+function removeIsTiny() {
+    detailsImage.parentElement.classList.remove('is-tiny');
+}
+
+function hideDetails() {
+    mainContentEl.classList.add('hidden') ;
+    if (selectedItem) {
+        selectedItem.classList.remove('selected')
+    }
 }
